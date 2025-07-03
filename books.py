fca64efc83b5de43f9b8df9829613aaffe63bff5
rajxyz <atptricks@gmail.com>
2025-07-03 20:00:31 +0530

    Update books.py

--- books.py
+++ books.py
@@ -1,1 +1,13 @@
+import os
+import json
 
+def get_chapter_pages(book, chapter):
+    folder_path = f"static/books/{book}/{chapter}"
+    pages = []
+    for img_file in sorted(os.listdir(folder_path)):
+        if img_file.endswith('.png'):
+            pages.append({
+                'page_number': img_file.split('.')[0],
+                'image': f"/static/books/{book}/{chapter}/{img_file}"
+            })
+    return pages
