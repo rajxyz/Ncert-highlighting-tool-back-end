2296fecf234b58163984cdd25e5dd5a154475504
rajxyz <atptricks@gmail.com>
2025-07-03 20:02:55 +0530

    Update pdf_parser.py

--- pdf_parser.py
+++ pdf_parser.py
@@ -1,1 +1,6 @@
+from pdf2image import convert_from_path
 
+def convert_pdf_to_images(pdf_path, output_folder):
+    pages = convert_from_path(pdf_path)
+    for i, page in enumerate(pages):
+        page.save(f"{output_folder}/page_{i+1}.png", 'PNG')
